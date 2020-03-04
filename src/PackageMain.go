ccfb37da4ec3a95338035f995e66993a0d51c5d7
huifer <huifer97@163.com>
2020-03-04 15:23:20 +0800

    go 结构体

--- src/PackageMain.go
+++ src/PackageMain.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"learn-go/src/goStrings"
-	"learn-go/src/pac"
-	"learn-go/src/pac/inpac"
 	_ "learn-go/src/pac/inpac"
+	"learn-go/src/structure"
+
+	_ "learn-go/src/structure"
 )
 
 func main() {
-	pac.Pac01()
-	inpac.Package02()
-	goStrings.UseStrings()
+	//pac.Pac01()
+	//inpac.Package02()
+	//goStrings.UseStrings()
+
+	user := structure.GetUser()
+	print(user.Age)
+	print(user.Name)
 
 }
