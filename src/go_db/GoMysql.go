f39aafa15a2f9e593735fe226196733a04e65e0a
wt <huifer97@163.com>
2020-06-11 18:04:42 +0800

    mysql

--- src/go_db/GoMysql.go
+++ src/go_db/GoMysql.go
@@ -17,7 +17,7 @@ type mysql_conf struct {
 }
 
 func conn(conf mysql_conf) *sql.DB {
-	url := conf.Username + ":" + conf.Password + "@tcp(" + conf.Host + ":" + strconv.Itoa(conf.Port) + ")/" + conf.DbName
+	url := conf.Username + ":" + conf.Password + "@tcp(" + conf.Host + ":" + strconv.Itoa(conf.Port) + ")/" + conf.DbName + "?charset=utf8mb4&multiStatements=true"
 	fmt.Println(url)
 	db, err := sql.Open("mysql", url)
 	if err != nil {
@@ -27,23 +27,70 @@ func conn(conf mysql_conf) *sql.DB {
 	return db
 }
 
+func insert(db *sql.DB, sql string) (int64, error) {
+	stmt, err := db.Prepare(sql)
+	checkErr(err)
+	exec, err := stmt.Exec("张三", 18)
+	checkErr(err)
+	return exec.LastInsertId()
+}
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type t_user struct {
+	Id   int
+	Name string
+	Age  int
+}
+
 func main() {
 	conf := mysql_conf{
-		Host:     "10.10.0.124",
+		Host:     "47.98.225.144",
 		Port:     3306,
-		Username: "shandsmod3",
-		Password: "shandsmod3",
-		DbName:   "shandsmod3",
+		Username: "huifer",
+		Password: "a12345",
+		DbName:   "scrum",
 	}
 
-	db := conn(conf)
-	query, err := db.Query("select * from hs_log")
+	//sql := "INSERT INTO `scrum`.`t_user`(`name`, `age`) VALUES (?, ?)"
+	//id, err := insert(conn(conf), sql)
+	//checkErr(err)
+	//sql = "select id , name ,age from t_user where id = ?"
+	//
+	//user := selectSql(conn(conf), sql, id)
+	//fmt.Println(user)
+	sql := "UPDATE `scrum`.`t_user` SET `age` = ? WHERE `id` = ?"
+	upId, err := updateSql(conn(conf), sql, 3, 12)
+	checkErr(err)
+
+	fmt.Println("id = ", upId)
+}
+
+func updateSql(db *sql.DB, sql string, id int64, age int) (int64, error) {
+	prepare, err := db.Prepare(sql)
 	if err != nil {
-		for query.Next() {
-			columns, err := query.Columns()
-			if err == nil {
-				print(columns)
-			}
-		}
+		panic(err)
 	}
+	exec, err := prepare.Exec(age, id)
+	if err != nil {
+		panic(err)
+	}
+	return exec.RowsAffected()
+}
+
+func selectSql(db *sql.DB, s string, id int64) t_user {
+	stmt, err := db.Prepare(s)
+	checkErr(err)
+	exec, err := stmt.Query(id)
+	checkErr(err)
+	user := t_user{}
+
+	for exec.Next() {
+		exec.Scan(&user.Id, &user.Name, &user.Age)
+	}
+	return user
+
 }
